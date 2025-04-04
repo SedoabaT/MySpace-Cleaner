958facff57a2af231c94c37f95a310049389fb7f
sedoa <sedoa@DESKTOP-T1G1256>
2025-04-04 16:46:19 +0200

    Add project files.

---
+++ MySpace Cleaner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MySpace_Cleaner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
